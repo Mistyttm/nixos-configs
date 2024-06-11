a1d8d520fd72c09b3d2e7b8b9ba05dcf4cb941ad
Mistyttm <contact@mistyttm.dev>
2024-06-11 17:25:11 +1000

    First Laptop Nix Install

---
+++ home.nix
@@ -0,0 +1,36 @@
+{ config, pkgs, ... }:
+{
+  # TODO please change the username & home directory to your own
+  home.username = "misty";
+  home.homeDirectory = "/home/misty";
+
+  # Packages that should be installed to the user profile.
+  home.packages = with pkgs; [
+    git
+    discord
+    firefox
+    vlc
+  ];
+  programs.kitty = {
+    enable = true;
+  };
+  programs.zsh = {
+    enable = true;
+  };
+  programs.oh-my-posh = {
+    enable = true;
+  };
+  # This value determines the home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update home Manager without changing this value. See
+  # the home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "24.05";
+
+  # Let home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+}
