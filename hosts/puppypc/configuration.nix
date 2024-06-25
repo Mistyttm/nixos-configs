40bf5529388dd49c5c84349c34d3f10993ba14b9
Mistyttm <contact@mistyttm.dev>
2024-06-25 21:39:05 +1000

    Added more system configs

--- hosts/puppypc/configuration.nix
+++ hosts/puppypc/configuration.nix
@@ -9,62 +9,16 @@
     [ # Include the results of the hardware scan.
       ./system/default.nix
       ./users/misty.nix
+      ./configs/gnupg/gnupg.nix
+      ../../global-configs/fonts/fonts.nix
     ];
 
   networking.hostName = "puppypc"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "Australia/Brisbane";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_AU.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_AU.UTF-8";
-    LC_IDENTIFICATION = "en_AU.UTF-8";
-    LC_MEASUREMENT = "en_AU.UTF-8";
-    LC_MONETARY = "en_AU.UTF-8";
-    LC_NAME = "en_AU.UTF-8";
-    LC_NUMERIC = "en_AU.UTF-8";
-    LC_PAPER = "en_AU.UTF-8";
-    LC_TELEPHONE = "en_AU.UTF-8";
-    LC_TIME = "en_AU.UTF-8";
-  };
-
-
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
   services.gnome.gnome-keyring.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
--- hosts/puppypc/system/default.nix
+++ hosts/puppypc/system/default.nix
@@ -1,10 +1,13 @@
 { config, lib, pkgs, ... }: {
   imports = [ # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    ./locale.nix
     ./programs.nix
     ./bootloader/bootloader.nix
     ./DE/default.nix
     ./hardware/amd.nix
     ./hardware/nvidia.nix
+    ./networking/networkmanager.nix
+    ./networking/ssh.nix
   ];
 }
