1f5dbf7df2f9beed97fd65d37346ff4164eed008
Mistyttm <contact@mistyttm.dev>
2024-06-25 19:56:23 +1000

    Discord is doin a fuckywucky

--- hosts/puppypc/configs/programs/common.nix
+++ hosts/puppypc/configs/programs/common.nix
@@ -2,6 +2,8 @@
   home.packages = with pkgs; [
     mullvad-vpn
     qbittorrent
+#     discord
+    vencord
     (pkgs.discord.override {
       # remove any overrides that you don't want
       withOpenASAR = true;
