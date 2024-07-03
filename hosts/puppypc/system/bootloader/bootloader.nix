4cb1fb701ae0efb3b3f5a20617b04e29b9697934
Mistyttm <contact@mistyttm.dev>
2024-07-03 17:08:55 +1000

    plymouth

--- hosts/puppypc/system/bootloader/bootloader.nix
+++ hosts/puppypc/system/bootloader/bootloader.nix
@@ -1,14 +1,44 @@
 { config, lib, pkgs, ... }: {
   # Bootloader.
-  boot.loader = {
-    efi = {
-      canTouchEfiVariables = true;
-    };
-    grub = {
+  boot = {
+    plymouth = {
       enable = true;
-      efiSupport = true;
-      devices = [ "nodev" ];
-      useOSProber = true;
+      theme = "cuts";
+      themePackages = with pkgs; [
+        # By default we would install all themes
+        (adi1090x-plymouth-themes.override {
+          selected_themes = [ "cuts" ];
+        })
+      ];
+      logo = pkgs.fetchurl {
+        url = "https://nixos.org/logo/nixos-hires.png";
+        sha256 = "1ivzgd7iz0i06y36p8m5w48fd8pjqwxhdaavc0pxs7w1g7mcy5si";
+      };
+    };
+
+    # Enable "Silent Boot"
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
+
+    loader = {
+      efi = {
+        canTouchEfiVariables = true;
+      };
+      grub = {
+        enable = true;
+        efiSupport = true;
+        devices = [ "nodev" ];
+        useOSProber = true;
+      };
     };
   };
 }
