4fc3c0a5b28adbbc7c32d231a351708d0ea657e6
Mistyttm <contact@mistyttm.dev>
2024-07-03 22:57:53 +1000

    GPG

--- hosts/puppypc/configs/programs/git.nix
+++ hosts/puppypc/configs/programs/git.nix
@@ -2,7 +2,13 @@
   programs.git = {
     signing = {
       key = "A1B4238FBD53F150";
+      gpgPath = "\${pkgs.gnupg}/bin/gpg2";
       signByDefault = true;
+    };
+    includes.*.contents = {
+      commit = {
+        gpgSign = true;
+      };
     };
   };
 }
