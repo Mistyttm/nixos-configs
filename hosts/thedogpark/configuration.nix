bc8a6b5ac06e916f400da2f0a035917770f8ca3a
Mistyttm <contact@mistyttm.dev>
2024-08-18 22:28:02 +1000

    putting it in the environment

--- hosts/thedogpark/configuration.nix
+++ hosts/thedogpark/configuration.nix
@@ -33,9 +33,10 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-#   environment.systemPackages = with pkgs; [
-#
-#   ];
+  environment.systemPackages = with pkgs; [
+    zip
+    unzip
+  ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
