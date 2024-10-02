f68db1149cd41651e53546b2248528825f82f949
Mistyttm <contact@mistyttm.dev>
2024-10-02 20:47:17 +1000

    ???

--- hosts/thedogpark/configuration.nix
+++ hosts/thedogpark/configuration.nix
@@ -30,7 +30,13 @@
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
+<<<<<<< Updated upstream
   sops.defaultSopsFile = ./secrets/porkbun.yaml;
+||||||| Stash base
+  sops.defaultSopsFile = ../../secrets/porkbun.yaml;
+=======
+  # sops.defaultSopsFile = ./secrets/porkbun.yaml;
+>>>>>>> Stashed changes
   sops.age.keyFile = "/var/lib/sops-nix/key.txt";
 
 
