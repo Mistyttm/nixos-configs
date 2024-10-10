49c20e525ecb01ae0745bdb355b64753ea596122
Mistyttm <contact@mistyttm.dev>
2024-10-11 00:30:11 +1000

    Remove display manager theme, just doesnt work

--- global-configs/system/DE/SDDM/displaymanager.nix
+++ global-configs/system/DE/SDDM/displaymanager.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, ... }: {
+{ pkgs, ... }: {
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
@@ -6,20 +6,7 @@
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm = {
     enable = true;
-#     sugarCandyNix = {
-#       enable = true; # This set SDDM's theme to "sddm-sugar-candy-nix".
-#       settings = {
-#         # Set your configuration options here.
-#         # Here is a simple example:
-#         Background = lib.cleanSource ./background.jpg;
-#         ScreenWidth = 1920;
-#         ScreenHeight = 1080;
-#         FormPosition = "left";
-#         HaveFormBackground = true;
-#         PartialBlur = true;
-#         # ...
-#       };
-#     };
+    # theme = "${pkgs.sddm-sugar-dark}/share/sddm/themes/sugar-dark";
   };
 }
 
