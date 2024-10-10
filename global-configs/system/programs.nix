96988eb4210884a0394bdfa745849cb4e3783586
Mistyttm <contact@mistyttm.dev>
2024-10-11 00:30:49 +1000

    Add sddm-sugar-dark

--- global-configs/system/programs.nix
+++ global-configs/system/programs.nix
@@ -8,7 +8,8 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    pkgs.qemu
+    qemu
+    sddm-sugar-dark
   ];
 
   programs.steam = {
