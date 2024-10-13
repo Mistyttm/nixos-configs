6191e7eab67dd24f04179700de6d5ac79ea67a89
Mistyttm <contact@mistyttm.dev>
2024-10-13 15:53:27 +1000

    New installPhase

--- hosts/swaytest/home.nix
+++ hosts/swaytest/home.nix
@@ -9,10 +9,36 @@
       hash = "sha256-T5/VnQgSaJF/WnYWxwI1DCtj2u0Ncoz++LGGcPj8xH8=";
     };  
     installPhase = ''
-      ls -a
-      mkdir -p $out/share/icons
-      cp -aR * $out/share/icons
-      $out/share/icons/install.sh -c purple
+       # Create the destination directory
+        mkdir -p $out/share/icons
+
+        # Define color and theme name
+        color="purple"
+        THEME_NAME="Tela-circle-$color"
+
+        # Create the theme directory
+        THEME_DIR="$out/share/icons/$THEME_NAME"
+        mkdir -p "$THEME_DIR"
+
+        # Copy the source files
+        cp -r src/* "$THEME_DIR"
+        cp -r links/* "$THEME_DIR"
+
+        # Update index.theme
+        sed -i "s/%NAME%/$THEME_NAME/g" "$THEME_DIR/index.theme"
+
+        # Apply color customization
+        find "$THEME_DIR" -type f -name "*.svg" -exec sed -i \
+          -e "s/#5294e2/#7e57c2/g" \
+          -e "/\ColorScheme-Highlight/s/currentColor/#7e57c2/" \
+          -e "/\ColorScheme-Background/s/currentColor/#ffffff/" {} +
+
+        # Create symlinks for HiDPI
+        ln -sr "$THEME_DIR/16" "$THEME_DIR/16@2x"
+        ln -sr "$THEME_DIR/22" "$THEME_DIR/22@2x"
+        ln -sr "$THEME_DIR/24" "$THEME_DIR/24@2x"
+        ln -sr "$THEME_DIR/32" "$THEME_DIR/32@2x"
+        ln -sr "$THEME_DIR/scalable" "$THEME_DIR/scalable@2x"
     '';
   };
  in{
