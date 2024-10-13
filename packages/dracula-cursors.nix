a0571c338d68c923e81dd31b29c5431bf1ef12f3
Mistyttm <contact@mistyttm.dev>
2024-10-13 18:04:25 +1000

    Change up the build

--- packages/dracula-cursors.nix
+++ packages/dracula-cursors.nix
@@ -8,15 +8,14 @@
       rev = "e5c95d75f4e04b91d9f47ff5395fcf0376cc045d";
       hash = "sha256-J3pOw5UCZgF3z76kSMQl179hbAPox60D0tkvBDMwn18=";
     };
-    buildInputs = [ pkgs.inkscape pkgs.xorg.xcursorgen ];
-    buildPhase = ''
-      chmod +x kde/cursors/build.sh
-      ls -l kde/cursors
-      exec kde/cursors/build.sh
-    '';
+
     installPhase = ''
       mkdir -p $out/share/icons/Dracula-cursors
       cd kde/cursors && ls
-      cp -r kde/cursors/Dracula-sdfsdfdf/* $out/share/icons/Dracula-cursors
+      cp -r kde/cursors/build/x2/* $out/share/icons/Dracula-cursors/cursors
+      touch $out/share/icons/Dracula-cursors/index.theme
+      echo "[Icon Theme]" >> $out/share/icons/Dracula-cursors/index.theme
+      echo "Name=Dracula-cursors" >> $out/share/icons/Dracula-cursors/index.theme
+      echo "Comment=dracula colors cursors" >> $out/share/icons/Dracula-cursors/index.theme
     '';
   }
