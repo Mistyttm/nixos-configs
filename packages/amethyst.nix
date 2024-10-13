a1eed967693a88b7fd180b17d111f9c790b110c0
Mistyttm <contact@mistyttm.dev>
2024-10-13 16:39:48 +1000

    add amethyst hash

--- packages/amethyst.nix
+++ packages/amethyst.nix
@@ -6,7 +6,7 @@
       repo = "Amethyst";
       owner = "ddh4r4m";
       rev = "a6af9b7727e0337b98a5705bc60b9a6b841761c7";
-      hash = "";
+      hash = "sha256-8F5CF+yzViyDUJuKDdbAP1KCgZx7HOqtJGWbxztBokk=";
     };  
     installPhase = ''
       mkdir -p $out/share/plasma
