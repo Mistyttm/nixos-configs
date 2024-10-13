5025788fd0daac86727b91aebac933bc71f7d6e0
Mistyttm <contact@mistyttm.dev>
2024-10-13 18:29:47 +1000

    Change repo name

--- packages/window-title-applet.nix
+++ packages/window-title-applet.nix
@@ -3,7 +3,7 @@
     pname = "plasma6-window-title-applet";
     version = "latest";
     src = pkgs.fetchFromGitHub {
-      repo = "Amethyst";
+      repo = "plasma6-window-title-applet";
       owner = "dhruv8sh";
       rev = "a6eaf5086a473919ed2fffc5d3b8d98237c2dd41";
       hash = "";
