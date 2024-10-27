0306e6ff8589555a234405a964a69711a1cf39a9
Mistyttm <contact@mistyttm.dev>
2024-10-27 12:40:32 +1000

    (puppypc/wivrn): Add wivrn

---
+++ hosts/puppypc/configs/programs/VR/wivrn.nix
@@ -0,0 +1,26 @@
+{ ... }: {
+    config.services.wivrn = {
+        enable = true;
+        openFirewall = true;
+        defaultRuntime = true;
+        autoStart = true;
+        config = {
+            enable = true;
+            json = {
+                scale = 1.0;
+                bitrate = 100000000;
+                encoders = [
+                    {
+                        encoder = "vaapi";
+                        codec = "h265";
+                        # 1.0 x 1.0 scaling
+                        width = 1.0;
+                        height = 1.0;
+                        offset_x = 0.0;
+                        offset_y = 0.0;
+                    }
+                ];
+            };
+        };
+    };
+}
