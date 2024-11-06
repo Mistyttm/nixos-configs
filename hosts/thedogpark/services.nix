73f1888286f46469120482a8b36b4b67ff3fd5ec
Mistyttm <contact@mistyttm.dev>
2024-11-06 17:03:05 +1000

    Run the server inside tmux

--- hosts/thedogpark/services.nix
+++ hosts/thedogpark/services.nix
@@ -1,4 +1,10 @@
-{ pkgs, ... }: {
+{ pkgs, ... }: let
+  valheimServiceScript = pkgs.writeScriptBin "valheimserver" ''
+    #!/bin/sh
+    # Start or attach to a tmux session for the Valheim server
+    tmux new-session -d -s valheimserver "/srv/valheim/docker_start_server.sh /srv/valheim/start_server.sh"
+  '';
+in{
   services = {
     gnome = {
       gnome-keyring = {
@@ -45,11 +51,12 @@
     after = [ "docker.service" ];
     requires = [ "docker.service" ];
 
-    path = with pkgs; [ docker ];
+    path = with pkgs; [ docker tmux ];
 
     # Set the command to run using ExecStart
     serviceConfig = {
-      ExecStart = "/srv/valheim/docker_start_server.sh /srv/valheim/start_server.sh";
+      ExecStart = "${valheimServiceScript}";
+      ExecStop = "tmux kill-session -t valheimserver";
       Restart = "always";
       RestartSec = "5s";
       WorkingDirectory = "/srv/valheim";
