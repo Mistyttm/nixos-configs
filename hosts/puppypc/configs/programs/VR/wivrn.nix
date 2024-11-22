db27ec4d57a604ad5a167d0ec30dde92ef25921c
Mistyttm <contact@mistyttm.dev>
2024-11-22 17:42:04 +1000

    fix: use nvenc encoder
    NVIDIA again

--- hosts/puppypc/configs/programs/VR/wivrn.nix
+++ hosts/puppypc/configs/programs/VR/wivrn.nix
@@ -8,17 +8,19 @@
         config = {
             enable = true;
             json = {
+                # 1.0x foveation scaling
                 scale = 1.0;
+                # 100 Mb/s
                 bitrate = 100000000;
                 encoders = [
                     {
-                        encoder = "vaapi";
-                        codec = "h265";
-                        # 1.0 x 1.0 scaling
-                        width = 1.0;
-                        height = 1.0;
-                        offset_x = 0.0;
-                        offset_y = 0.0;
+                    encoder = "nvenc";
+                    codec = "h265";
+                    # 1.0 x 1.0 scaling
+                    width = 1.0;
+                    height = 1.0;
+                    offset_x = 0.0;
+                    offset_y = 0.0;
                     }
                 ];
             };
