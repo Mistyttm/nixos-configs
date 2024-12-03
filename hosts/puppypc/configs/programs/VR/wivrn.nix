ac8fb13e17d31e785343e4e788b963212daf6337
Mistyttm <contact@mistyttm.dev>
2024-12-03 14:18:11 +1000

    fix(puppypc/vr): Fix VR configs

--- hosts/puppypc/configs/programs/VR/wivrn.nix
+++ hosts/puppypc/configs/programs/VR/wivrn.nix
@@ -1,15 +1,13 @@
 { pkgs, ... }: {
     services.wivrn = {
         enable = true;
-        package = pkgs.wivrnupdate-nixpkgs.wivrn;
+        package = pkgs.wivrn;
         openFirewall = true;
         defaultRuntime = true;
         autoStart = true;
         config = {
             enable = true;
             json = {
-                # 1.0x foveation scaling
-                scale = 1.0;
                 # 50 Mb/s
                 bitrate = 50000000;
                 encoders = [
