50fc6ab7defb0b85e19c7c852770fefc8d35c417
Mistyttm <contact@mistyttm.dev>
2025-01-10 19:23:11 +1000

    fix(thedogpark): add myself to minecraft user group

--- hosts/thedogpark/configuration.nix
+++ hosts/thedogpark/configuration.nix
@@ -76,26 +76,12 @@
   networking.firewall.allowedUDPPortRanges = [
     { from = 2456; to = 2458; }
   ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  
 
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  users.users.misty = {
+    extraGroups = [ "minecraft" ];
+  };
+
   system.stateVersion = "unstable"; # Did you read the comment?
 
 }
