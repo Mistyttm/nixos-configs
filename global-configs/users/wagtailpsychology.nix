f7e5c733bda4a580048d050af67b764013a774a7
Mistyttm <contact@mistyttm.dev>
2025-03-19 09:29:50 +1000

    chore(users): Remove unused definitions

--- global-configs/users/wagtailpsychology.nix
+++ global-configs/users/wagtailpsychology.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, ... }: {
+{ pkgs, ... }: {
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.wagtailpsychology = {
     isNormalUser = true;
