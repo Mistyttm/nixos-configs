e491012b0740063531013175e7c483c682c0b439
Mistyttm <contact@mistyttm.dev>
2025-03-19 09:45:51 +1000

    chore(puppypc/home): Remove commented out XDG config

--- hosts/puppypc/home.nix
+++ hosts/puppypc/home.nix
@@ -22,31 +22,4 @@
   nix.settings = {
     experimental-features = [ "nix-command" "flakes" ];
   };
-
-  # NOTE: move this to seperate file. Temporary
-  # xdg = {
-  #       configFile = {
-  #           "openxr/1/active_runtime.json".source = "${pkgs.wivrn}/share/openxr/1/openxr_wivrn.json";
-  #           "openvr/openvrpaths.vrpath".text = ''
-  #           {
-  #               "config" :
-  #               [
-  #                   "${config.xdg.dataHome}/Steam/config"
-  #               ],
-  #               "external_drivers" : null,
-  #               "jsonid" : "vrpathreg",
-  #               "log" :
-  #               [
-  #                   "${config.xdg.dataHome}/Steam/logs"
-  #               ],
-  #               "runtime" :
-  #               [
-  #                   "${pkgs.opencomposite}/lib/opencomposite"
-  #               ],
-  #               "version" : 1
-  #           }
-  #           '';
-  #       };
-  #   };
-
 }
