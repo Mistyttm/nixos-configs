e30dc3e526da34dfcd4e44834487dba7adc9849e
Mistyttm <contact@mistyttm.dev>
2025-04-02 17:00:23 +1000

    feat(thekennel): Add boot

--- hosts/thekennel/configuration.nix
+++ hosts/thekennel/configuration.nix
@@ -17,7 +17,7 @@
   # boot.loader.grub.efiInstallAsRemovable = true;
   # boot.loader.efi.efiSysMountPoint = "/boot/efi";
   # Define on which hard drive you want to install Grub.
-  # boot.loader.grub.device = "/dev/vda"; # or "nodev" for efi only
+  boot.loader.grub.device = "nodev"; # or "nodev" for efi only
 
   networking.hostName = "thekennel"; # Define your hostname.
   # Pick only one of the below networking options.
