fa9509ebbc00a56050e6f215ee549daacc9432ff
Mistyttm <contact@mistyttm.dev>
2025-04-02 16:51:39 +1000

    feat(thekennel): Add the Kennel server

---
+++ hosts/thekennel/configuration.nix
@@ -0,0 +1,65 @@
+{ pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ../../global-configs/users/misty.nix
+      ../../global-configs/system/locale.nix
+      ../../global-configs/system/networking/ssh.nix
+      ../../global-configs/system/nixoptions.nix
+      ../../global-configs/system/virtualisation.nix
+    ];
+
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.efiSupport = true;
+  # boot.loader.grub.efiInstallAsRemovable = true;
+  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  # Define on which hard drive you want to install Grub.
+  # boot.loader.grub.device = "/dev/vda"; # or "nodev" for efi only
+
+  networking.hostName = "thekennel"; # Define your hostname.
+  # Pick only one of the below networking options.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  # sops.defaultSopsFile = ./secrets/porkbun.yaml;
+
+  programs.nix-ld.enable = true;
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    zip
+    unzip
+    nano
+    tmux
+    sops
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  programs.mtr.enable = true;
+  programs.gnupg = {
+    agent = {
+      enable = true;
+      enableSSHSupport = true;
+      pinentryPackage = pkgs.pinentry-gnome3;
+    };
+  };
+
+
+  # List services that you want to enable:
+
+  nixpkgs.config.allowUnfree = true;
+
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
+
---
+++ hosts/thekennel/home.nix
@@ -0,0 +1,39 @@
+{ pkgs, ... }:
+{
+  # TODO please change the username & home directory to your own
+  home.username = "misty";
+  home.homeDirectory = "/home/misty";
+
+  imports = [
+  ../../global-configs/programs/git.nix
+  ../../global-configs/programs/xdg.nix
+  ../../global-configs/programs/cli.nix
+  ../../global-configs/programs/fastfetch.nix
+  ];
+
+  # Packages that should be installed to the user profile.
+  home.packages = with pkgs; [
+    direnv
+    nil
+  ];
+  
+  # This value determines the home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update home Manager without changing this value. See
+  # the home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "24.11";
+
+  # Let home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  programs.java.enable = true;
+
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+  };
+
+}
