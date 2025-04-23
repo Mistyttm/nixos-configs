ec11bf6e46b92ed239a620ce1b6ff17767428fd0
Mistyttm <contact@mistyttm.dev>
2025-04-23 11:38:20 +1000

    no sops

--- hosts/thekennel/services/jellyfin.nix
+++ hosts/thekennel/services/jellyfin.nix
@@ -1,27 +1,27 @@
 { pkgs, config, ... }: 
 {
-  sops.secrets."nas-username" = {
-    sopsFile = ../../secrets/smb-creds.yaml;
-    owner = "root";
-  };
-  sops.secrets."nas-password" = {
-    sopsFile = ../../secrets/smb-creds.yaml;
-    owner = "root";
-  };
+  # sops.secrets."nas-username" = {
+  #   sopsFile = ../../secrets/smb-creds.yaml;
+  #   owner = "root";
+  # };
+  # sops.secrets."nas-password" = {
+  #   sopsFile = ../../secrets/smb-creds.yaml;
+  #   owner = "root";
+  # };
 
   # Systemd service to generate /run/media-credentials at runtime
-  systemd.services."generate-media-creds" = {
-    wantedBy = [ "local-fs.target" ];
-    before = [ "mnt-media.mount" ];
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = pkgs.writeShellScript "gen-creds" ''
-        install -m 600 -o root -g root /dev/null /run/media-credentials
-        echo "username=$(cat ${config.sops.secrets."nas-username".path})" > /run/media-credentials
-        echo "password=$(cat ${config.sops.secrets."nas-password".path})" >> /run/media-credentials
-      '';
-    };
-  };
+  # systemd.services."generate-media-creds" = {
+  #   wantedBy = [ "local-fs.target" ];
+  #   before = [ "mnt-media.mount" ];
+  #   serviceConfig = {
+  #     Type = "oneshot";
+  #     ExecStart = pkgs.writeShellScript "gen-creds" ''
+  #       install -m 600 -o root -g root /dev/null /run/media-credentials
+  #       echo "username=$(cat ${config.sops.secrets."nas-username".path})" > /run/media-credentials
+  #       echo "password=$(cat ${config.sops.secrets."nas-password".path})" >> /run/media-credentials
+  #     '';
+  #   };
+  # };
 
   services.jellyfin = {
     enable = true;
