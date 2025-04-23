faafce035eb8275be6713ed09a28549f324126de
Mistyttm <contact@mistyttm.dev>
2025-04-23 17:10:24 +1000

    refactor: open firewall

--- hosts/thekennel/services/jellyfin.nix
+++ hosts/thekennel/services/jellyfin.nix
@@ -1,28 +1,5 @@
 { pkgs, config, ... }: 
 {
-  # sops.secrets."nas-username" = {
-  #   sopsFile = ../../secrets/smb-creds.yaml;
-  #   owner = "root";
-  # };
-  # sops.secrets."nas-password" = {
-  #   sopsFile = ../../secrets/smb-creds.yaml;
-  #   owner = "root";
-  # };
-
-  # # Systemd service to generate /run/media-credentials at runtime
-  # systemd.services."generate-media-creds" = {
-  #   wantedBy = [ "local-fs.target" ];
-  #   before = [ "mnt-media.mount" ];
-  #   serviceConfig = {
-  #     Type = "oneshot";
-  #     ExecStart = pkgs.writeShellScript "gen-creds" ''
-  #       install -m 600 -o root -g root /dev/null /run/media-credentials
-  #       echo "username=$(cat ${config.sops.secrets."nas-username".path})" > /run/media-credentials
-  #       echo "password=$(cat ${config.sops.secrets."nas-password".path})" >> /run/media-credentials
-  #     '';
-  #   };
-  # };
-
   environment.systemPackages = with pkgs; [
     jellyfin
     jellyfin-web
@@ -32,6 +9,7 @@
   services.jellyfin = {
     enable = true;
     package = pkgs.jellyfin;
+    openFirewall = true;
     user = "jellyfin";
     group = "jellyfin";
     dataDir = "/var/lib/jellyfin";
