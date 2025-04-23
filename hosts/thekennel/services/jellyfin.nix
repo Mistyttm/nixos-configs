02349b0373551381e11ff3c272a4ad44fa807711
Mistyttm <contact@mistyttm.dev>
2025-04-23 11:34:48 +1000

    refactor: remove mediaDirs

--- hosts/thekennel/services/jellyfin.nix
+++ hosts/thekennel/services/jellyfin.nix
@@ -30,21 +30,20 @@
     group = "jellyfin";
     dataDir = "/var/lib/jellyfin";
     configDir = "/var/lib/jellyfin/config";
-    mediaDirs = [ "/mnt/media" ];
   };
 
   # Enable Remote Storage
-  fileSystems."/mnt/media" = {
-    device = "//192.168.0.170/Public/Media";
-    fsType = "nfs";
-    options = [
-      "credentials=/run/media-credentials" 
-      "uid=1000"
-      "gid=100" 
-      "iocharset=utf8"
-      "vers=3.0"
-      "x-systemd.automount"
-      "noauto"
-    ];
-  };
+  # fileSystems."/mnt/media" = {
+  #   device = "//192.168.0.170/Public/Media";
+  #   fsType = "nfs";
+  #   options = [
+  #     "credentials=/run/media-credentials" 
+  #     "uid=1000"
+  #     "gid=100" 
+  #     "iocharset=utf8"
+  #     "vers=3.0"
+  #     "x-systemd.automount"
+  #     "noauto"
+  #   ];
+  # };
 }
