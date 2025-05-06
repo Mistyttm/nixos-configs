eb4c25be2777bb4f17b51046dc943ccc5c0d3021
Mistyttm <contact@mistyttm.dev>
2025-05-06 19:22:55 +1000

    refactor: adjust tabs to spaces, indent of 2

--- hosts/thedogpark/matrix-synapse.nix
+++ hosts/thedogpark/matrix-synapse.nix
@@ -1,58 +1,67 @@
 { pkgs, lib, config, ... }: let 
-	maybeListener = lib.lists.findFirst (l: l.tls == false) null config.services.matrix-synapse.settings.listeners;
-  	synapsePort = if maybeListener == null then 8008 else maybeListener.port;
+  maybeListener = lib.lists.findFirst (l: l.tls == false) null config.services.matrix-synapse.settings.listeners;
+    synapsePort = if maybeListener == null then 8008 else maybeListener.port;
 in {
-	sops.secrets."postgresql_discord_password" = {
-    		sopsFile = ../../secrets/discordpostgresql.yaml;
-    		owner = config.users.users.mautrix-discord.name;
-    		group = config.users.users.mautrix-discord.group;
-  	};
+  sops.secrets."postgresql_discord_password" = {
+        sopsFile = ../../secrets/discordpostgresql.yaml;
+        owner = config.users.users.mautrix-discord.name;
+        group = config.users.users.mautrix-discord.group;
+    };
 
-	services.matrix-synapse = {
-		enable =true;
-		extras = [
-			"cache-memory" # Provide statistics about caching memory consumption
-			"jwt"          # JSON Web Token authentication
-			"oidc"         # OpenID Connect authentication
-			"postgres"     # PostgreSQL database backend
-			"saml2"        # SAML2 authentication
-			"sentry"       # Error tracking and performance metrics
-			"systemd"      # Provide the JournalHandler used in the default log_config
-			"url-preview"  # Support for oEmbed URL previews
-			"user-search"  # Support internationalized domain names in user-search
-		];
+  services.matrix-synapse = {
+    enable =true;
+    extras = [
+      "cache-memory" # Provide statistics about caching memory consumption
+      "jwt"          # JSON Web Token authentication
+      "oidc"         # OpenID Connect authentication
+      "postgres"     # PostgreSQL database backend
+      "saml2"        # SAML2 authentication
+      "sentry"       # Error tracking and performance metrics
+      "systemd"      # Provide the JournalHandler used in the default log_config
+      "url-preview"  # Support for oEmbed URL previews
+      "user-search"  # Support internationalized domain names in user-search
+    ];
 
-	};
-	services.mautrix-discord = {
-		enable = true;
-		settings = {
-			homeserver = {
-				address = "http://localhost:${synapsePort}";
-				domain = "mistyttm.dev";
-			};
-			appservice = {
-				database = {
-					uri = "postgres://mautrix:${config.sops.secrets.postgresql_discord_password.contents}@localhost/mautrix-discord?sslmode=disable";
-				};
-			};
-			bridge = {
-				direct-media = {
-					server_name = "discord-media.mistyttm.dev";
-				};
-				permissions = {
-					"mistyttm.dev" = "user";
-				};
-			};
-		};
-	};
-	services.postgresql = {
-  		enable = true;
-  		ensureDatabases = [ "mautrix-discord" ];
-  		ensureUsers = [
-    			{
-      				name = "mautrix-discord";
-      				ensurePermissions."DATABASE \"mautrix-discord\"" = "ALL PRIVILEGES";
-    			}
-  		];
-	};
+  };
+
+  users.user.synapse = {
+      isSystemUser = true;
+      group = "synapse";
+      # home = dataDir;
+      description = "Synapse user";
+  };
+
+  services.mautrix-discord = {
+    enable = true;
+    settings = {
+      homeserver = {
+        address = "http://localhost:${synapsePort}";
+        domain = "mistyttm.dev";
+      };
+      appservice = {
+        database = {
+          uri = "postgres://mautrix:${config.sops.secrets.postgresql_discord_password.contents}@localhost/mautrix-discord?sslmode=disable";
+        };
+      };
+      bridge = {
+        direct-media = {
+          server_name = "discord-media.mistyttm.dev";
+        };
+        permissions = {
+          "mistyttm.dev" = "user";
+        };
+      };
+    };
+  };
+
+  services.postgresql = {
+    enable = true;
+    ensureDatabases = [ "mautrix-discord" ];
+    ensureUsers = [
+      {
+        name = "mautrix-discord";
+        ensurePermissions."DATABASE \"mautrix-discord\"" = "ALL PRIVILEGES";
+      }
+    ];
+  };
 }
