af3edaedea765145ac9aa70d1ee7df04acd47047
Mistyttm <contact@mistyttm.dev>
2025-05-07 16:36:03 +1000

    chore: add forwards

--- hosts/thedogpark/mailserver.nix
+++ hosts/thedogpark/mailserver.nix
@@ -9,6 +9,13 @@
     enable = true;
     fqdn = "mail.mistyttm.dev";
     domains = [ "mistyttm.dev" ];
+    forwards = {
+      "contact@mistyttm.dev" = "emmey.leo@gmail.com";
+      "junk@mistyttm.dev" = "emmey.leo@gmail.com";
+      "school@mistyttm.dev" = "emmey.leo@connect.qut.edu.au";
+      "personal@mistyttm.dev" = "emmey.leo@gmail.com";
+      "secondary@mistyttm.dev" = "elijah.r.leo@gmail.com";
+    };
     # vmailGroupName = "vmail";
     loginAccounts = {
       "noreply@mistyttm.dev" = {
