5551c3acd24add354f415ff8a322b31787875411
Mistyttm <contact@mistyttm.dev>
2025-05-12 22:01:55 +1000

    chore: turn off emacs

--- hosts/puppypc/configs/programs/development.nix
+++ hosts/puppypc/configs/programs/development.nix
@@ -8,7 +8,7 @@
   ];
 
   programs.emacs = {
-    enable = true;
+    enable = false;
     package = pkgs.emacs;  # replace with pkgs.emacs-gtk, or a version provided by the community overlay if desired.
   };
 }
