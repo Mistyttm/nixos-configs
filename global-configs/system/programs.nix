31bfe8d690a74f8e6683c7b61c833501e2da4e9f
Mistyttm <contact@mistyttm.dev>
2025-08-24 18:41:04 +1000

    fix: Trickle build failure
    Fix and patch found from NixOS/nixpkgs#368974 by @arilotter
    
    NOTE TO SELF: Consider maybe looking at fixing this package yourself if you find use from it? It is over 12 years old and unmaintained

--- global-configs/system/programs.nix
+++ global-configs/system/programs.nix
@@ -18,6 +18,23 @@
     nil
     bitwarden-desktop
     bitwarden-cli
+    (trickle.overrideAttrs (oldAttrs: {
+      preConfigure = ''
+        sed -i 's|libevent.a|libevent.so|' configure
+        sed -i 's/if test "$HAVEMETHOD" = "no"; then/if false; then/' configure
+        sed -i 's/exit(1)/exit(0)/' configure
+        sed -i '1i#define DLOPENLIBC "${pkgs.stdenv.cc.libc}/lib/libc.so.6"' trickle-overload.c
+      '';
+      env = (oldAttrs.env or {}) // {
+        NIX_CFLAGS_COMPILE = toString ([
+          "-I${pkgs.libtirpc.dev}/include/tirpc"
+          "-Wno-pointer-sign"
+        ]);
+      };
+      patches = (oldAttrs.patches or []) ++ [
+        ../../patches/trickle/fix-build-failure.patch
+      ];
+    }))
   ];
 
   programs.zsh.enable = true;
