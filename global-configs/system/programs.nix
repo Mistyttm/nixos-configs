675f61398702685c326b8670332569863bb3bd1b
Mistyttm <contact@mistyttm.dev>
2025-09-02 20:21:20 +1000

    feat: Add Trickle and configs

--- global-configs/system/programs.nix
+++ global-configs/system/programs.nix
@@ -2,23 +2,8 @@
   pkgs,
   ...
 }:
-{
-  # Install firefox.
-  programs.firefox.enable = true;
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    #     qemu
-    sddm-sugar-dark
-    libaccounts-glib
-    nil
-    bitwarden-desktop
-    bitwarden-cli
-    (trickle.overrideAttrs (oldAttrs: {
+let
+trickleOverlay = (pkgs.trickle.overrideAttrs (oldAttrs: {
       preConfigure = ''
         sed -i 's|libevent.a|libevent.so|' configure
         sed -i 's/if test "$HAVEMETHOD" = "no"; then/if false; then/' configure
@@ -34,9 +19,52 @@
       patches = (oldAttrs.patches or []) ++ [
         ../../patches/trickle/fix-build-failure.patch
       ];
-    }))
+    }));
+in {
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    #     qemu
+    sddm-sugar-dark
+    libaccounts-glib
+    nil
+    bitwarden-desktop
+    bitwarden-cli
+    trickleOverlay
   ];
 
   programs.zsh.enable = true;
   users.users.misty.shell = pkgs.zsh;
+
+  environment.etc."trickled.conf".text = ''
+    [vesktop]
+    Priority = 1
+    Time-Smoothing = 0.1
+    Length-Smoothing = 2
+    Upload = 2500
+    Download = 0
+  '';
+
+  systemd.services.trickled = {
+    description = "Trickle bandwidth limiter daemon";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      # Run in foreground mode, let systemd handle daemonization
+      ExecStart = "${trickleOverlay}/bin/trickled -f -c /etc/trickled.conf";
+      Type = "simple";  # Changed from "forking" to "simple"
+      # Removed PIDFile since we're using Type=simple
+      Restart = "on-failure";
+      RestartSec = "5s";
+      # Create runtime directory for any files trickled might need
+      RuntimeDirectory = "trickled";
+      RuntimeDirectoryMode = "0755";
+    };
+  };
 }
