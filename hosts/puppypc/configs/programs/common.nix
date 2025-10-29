979e9f2af2abf050be746d5d11c71c9fffa04717
Mistyttm <contact@mistyttm.dev>
2025-10-29 17:37:13 +1000

    feat: Add linux-wallpaperengine config

--- hosts/puppypc/configs/programs/common.nix
+++ hosts/puppypc/configs/programs/common.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ pkgs, config, ... }: {
   home.packages = with pkgs; [
     mullvad-vpn
     qbittorrent
@@ -22,4 +22,20 @@
     plasma-panel-colorizer
     r2modman
   ];
+
+  services.linux-wallpaperengine = {
+    enable = true;
+    assetsPath = "${config.xdg.dataHome}/Steam/steamapps/common/wallpaper_engine/assets";
+    wallpapers = [
+      {
+        monitor = "DP-5";
+        wallpaperId = "3328281976";
+      }
+      {
+        monitor = "DP-4";
+        wallpaperId = "3215623224";
+        extraOptions = [ "--silent" ];
+      }
+    ];
+  };
 }
