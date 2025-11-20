bfefe6082d3b8f5ae9e3fb92f74ea029f8767df6
Mistyttm <contact@mistyttm.dev>
2025-11-20 14:56:39 +1000

    refactor: remove trickleOverlay configuration and related comments from programs.nix

--- global-configs/system/programs.nix
+++ global-configs/system/programs.nix
@@ -2,26 +2,7 @@
   pkgs,
   ...
 }:
-let
-trickleOverlay = (pkgs.trickle.overrideAttrs (oldAttrs: {
-      preConfigure = ''
-        sed -i 's|libevent.a|libevent.so|' configure
-        sed -i 's/if test "$HAVEMETHOD" = "no"; then/if false; then/' configure
-        sed -i 's/exit(1)/exit(0)/' configure
-        sed -i '1i#define DLOPENLIBC "${pkgs.stdenv.cc.libc}/lib/libc.so.6"' trickle-overload.c
-      '';
-      env = (oldAttrs.env or {}) // {
-        NIX_CFLAGS_COMPILE = toString ([
-          "-I${pkgs.libtirpc.dev}/include/tirpc"
-          "-Wno-pointer-sign"
-        ]);
-      };
-      patches = (oldAttrs.patches or []) ++ [
-        ../../patches/trickle/fix-build-failure.patch
-      ];
-    }));
-in {
-  # Install firefox ESR to avoid build reference issues
+{
   programs.firefox = {
     enable = true;
     package = pkgs.firefox-esr;
@@ -39,35 +20,8 @@ in {
     nil
     bitwarden-desktop
     bitwarden-cli
-    trickleOverlay
   ];
 
   programs.zsh.enable = true;
   users.users.misty.shell = pkgs.zsh;
-
-  environment.etc."trickled.conf".text = ''
-    [vesktop]
-    Priority = 1
-    Time-Smoothing = 0.1
-    Length-Smoothing = 2
-    Upload = 2500
-    Download = 0
-  '';
-
-#   systemd.services.trickled = {
-#     description = "Trickle bandwidth limiter daemon";
-#     after = [ "network.target" ];
-#     wantedBy = [ "multi-user.target" ];
-#     serviceConfig = {
-#       # Run in foreground mode, let systemd handle daemonization
-#       ExecStart = "${trickleOverlay}/bin/trickled -f -c /etc/trickled.conf";
-#       Type = "simple";  # Changed from "forking" to "simple"
-#       # Removed PIDFile since we're using Type=simple
-#       Restart = "on-failure";
-#       RestartSec = "5s";
-#       # Create runtime directory for any files trickled might need
-#       RuntimeDirectory = "trickled";
-#       RuntimeDirectoryMode = "0755";
-#     };
-#   };
 }
