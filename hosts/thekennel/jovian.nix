d5b41ba8e4165a410ebf7c89638cb938908c6d70
Mistyttm <contact@mistyttm.dev>
2025-11-20 22:22:37 +1000

    feat: add jovian configuration for thekennel host

---
+++ hosts/thekennel/jovian.nix
@@ -0,0 +1,22 @@
+{ ... }: {
+  jovian = {
+    steam = {
+      enable = true;
+      autoStart = true;
+      user = "misty";
+      desktopSession = "plasma";
+    };
+    decky-loader = {
+      enable = true;
+
+    };
+    steamos = {
+      enableBluetoothConfig = true;
+      enableDefaultCmdlineConfig = true;
+      enableEarlyOOM = true;
+      enableProductSerialAccess = true;
+      enableSysctlConfig = true;
+      enableZram = true;
+    };
+  };
+}
