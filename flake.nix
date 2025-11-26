1f8d817cb2c7d3bf4bf3c7b4b81d01c21349224d
Mistyttm <contact@mistyttm.dev>
2025-11-26 20:18:02 +1000

    fix: update Cachix cache name in build workflows and flake configuration

--- flake.nix
+++ flake.nix
@@ -43,7 +43,7 @@
   nixConfig = {
     extra-substituters = [
       "https://nix-community.cachix.org"
-      "https://mistyttm.cachix.org"
+      "https://opinionatedcache.cachix.org"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
