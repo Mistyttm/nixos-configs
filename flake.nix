70260a8c5d8fea8ebcb614c11f4dee382de34323
Mistyttm <contact@mistyttm.dev>
2025-12-04 23:20:27 +1000

    chore(flake): add nixos-cuda cache

--- flake.nix
+++ flake.nix
@@ -49,11 +49,13 @@
       "https://nix-community.cachix.org"
       "https://opinionatedcache.cachix.org"
       "https://cache.flox.dev"
+      "https://cache.nixos-cuda.org"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "opinionatedcache.cachix.org-1:zDO4tZBL25vfhVFSHTT+0RNCjn5Z5nEs7sPiDZ6XhuE="
       "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
+      "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
     ];
   };
 
