a116cf70cc7c462587c865a392ad8ed03cae96a6
Mistyttm <contact@mistyttm.dev>
2025-12-06 20:44:44 +1000

    refactor(thekennel): desktop nvidia card

--- hosts/thekennel/system/nvidia.nix
+++ hosts/thekennel/system/nvidia.nix
@@ -12,34 +12,16 @@
 
   hardware.nvidia = {
 
-    # Modesetting is required.
     modesetting.enable = true;
 
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
-    # of just the bare essentials.
     powerManagement.enable = false;
 
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
     powerManagement.finegrained = false;
 
-    open = false;
-
-    prime = {
-      intelBusId = "PCI:0:2:0";
-      nvidiaBusId = "PCI:1:0:0";
-      offload = {
-        enable = true;
-        enableOffloadCmd = true;
-      };
-    };
-
-    nvidiaPersistenced = true;
+    open = true;
 
     nvidiaSettings = true;
 
-    package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
   };
 }
