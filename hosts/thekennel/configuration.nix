69403de71df3e4aee631399c9152acfea1df5d5f
Mistyttm <contact@mistyttm.dev>
2025-12-06 23:11:19 +1000

    feat(thekennel): enable arr stack

--- hosts/thekennel/configuration.nix
+++ hosts/thekennel/configuration.nix
@@ -14,7 +14,7 @@
     ../../global-configs/system/DE/audio.nix
     ./services/jellyfin.nix
     ./system/nvidia.nix
-    # ./services/arr/default.nix
+    ./services/arr/default.nix
   ];
 
   # Bootloader.
