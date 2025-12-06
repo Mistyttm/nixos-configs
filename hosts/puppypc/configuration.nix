c3a9df9f86622cc47800f888fa9030ad5636ae6e
Mistyttm <contact@mistyttm.dev>
2025-12-06 23:06:33 +1000

    feat(puppypc): systemd-resolved

--- hosts/puppypc/configuration.nix
+++ hosts/puppypc/configuration.nix
@@ -148,6 +148,22 @@ in
     };
   };
 
+  networking.nameservers = [
+    "1.1.1.1"
+    "1.0.0.1"
+  ];
+
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    domains = [ "~." ];
+    fallbackDns = [
+      "1.1.1.1"
+      "1.0.0.1"
+    ];
+    dnsovertls = "true";
+  };
+
   system.stateVersion = "25.11";
 
 }
