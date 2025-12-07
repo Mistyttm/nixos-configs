bd95e35e21b5672ee20fc2248b08ebf1b1ce4b38
Mistyttm <contact@mistyttm.dev>
2025-12-07 21:46:45 +1000

    feat(thekennel): proper qbittorrent configuration

--- hosts/thekennel/services/arr/qbitorrent.nix
+++ hosts/thekennel/services/arr/qbitorrent.nix
@@ -10,6 +10,15 @@
         TempPathEnabled = true;
         TorrentExportDir = "/mnt/localExpansion/qbittorrent/torrents";
       };
+      Preferences = {
+        WebUI = {
+          Enabled = true;
+          Username = "admin";
+          Password_PBKDF2 = ''"@ByteArray(DiKrHs4S4j4pgnbP0+Vung==:eOcLXZTTGJqkayOBJJwaJM2dveNffFuRxcxsL57cliBfLtsHADg3xrd4w/HNy6TUSnBk1k2zJ3bof9dECXYHqg==)"'';
+          AuthSubnetWhitelist = "@Invalid()";
+          Port = 8080;
+        };
+      };
     };
   };
 }
