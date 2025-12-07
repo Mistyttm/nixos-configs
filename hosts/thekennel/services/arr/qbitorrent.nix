268640c5cba846a90976679be0a96e3f3c8657fc
Mistyttm <contact@mistyttm.dev>
2025-12-07 14:28:28 +1000

    fix(thekennel): qbittorrent download paths

--- hosts/thekennel/services/arr/qbitorrent.nix
+++ hosts/thekennel/services/arr/qbitorrent.nix
@@ -3,5 +3,16 @@
   services.qbittorrent = {
     enable = true;
     openFirewall = true;
+    serverConfig = {
+      # Set default save path
+      "Downloads/SavePath" = "/mnt/localExpansion/qbittorrent/downloads";
+
+      # Set temp path for incomplete downloads
+      "Downloads/TempPath" = "/mnt/localExpansion/qbittorrent/incomplete";
+      "Downloads/TempPathEnabled" = true;
+
+      # Optional: Set path for .torrent files
+      "Downloads/TorrentExportDir" = "/mnt/localExpansion/qbittorrent/torrents";
+    };
   };
 }
