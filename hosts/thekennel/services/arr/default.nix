1e5032cbfd366dc47070026345bf1aff42518c3d
Mistyttm <contact@mistyttm.dev>
2025-12-16 22:34:41 +1000

    fix(thekennel): fix nas mount

--- hosts/thekennel/services/arr/default.nix
+++ hosts/thekennel/services/arr/default.nix
@@ -9,4 +9,10 @@
     ./flaresolverr.nix
     ./jellyseer.nix
   ];
+
+  users.groups.media = { };
+
+  users.users.jellyfin.extraGroups = [ "media" ];
+  users.users.sonarr.extraGroups = [ "media" ];
+  users.users.radarr.extraGroups = [ "media" ];
 }
