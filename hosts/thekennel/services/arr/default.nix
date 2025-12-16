ea8827db773e260bcdc0d8db3194bf3fd2371552
Mistyttm <contact@mistyttm.dev>
2025-12-16 22:49:26 +1000

    feat(secrets): Add proper encrypted qnap secrets

--- hosts/thekennel/services/arr/default.nix
+++ hosts/thekennel/services/arr/default.nix
@@ -10,6 +10,19 @@
     ./jellyseer.nix
   ];
 
+  sops.secrets.qnap-media = {
+    owner = "root";
+    group = "root";
+    mode = "0400";
+    format = "yaml";
+  };
+
+  system.activationScripts.qnapMediaCreds.text = ''
+    install -m 0400 /dev/null /run/secrets/qnap-media.cifs
+    echo "username=$(yq -r .username /run/secrets/qnap-media)" >> /run/secrets/qnap-media.cifs
+    echo "password=$(yq -r .password /run/secrets/qnap-media)" >> /run/secrets/qnap-media.cifs
+  '';
+
   users.groups.media = { };
 
   users.users.jellyfin.extraGroups = [ "media" ];
