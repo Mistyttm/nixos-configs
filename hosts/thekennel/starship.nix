abce97ebb15b5b9f3f6d5690c08eb9cbb1a160d1
Mistyttm <contact@mistyttm.dev>
2025-12-17 16:51:14 +1000

    fix(thekennel): add hostname to starship

--- hosts/thekennel/starship.nix
+++ hosts/thekennel/starship.nix
@@ -4,7 +4,12 @@
     settings = {
       "$schema" = "https://starship.rs/config-schema.json";
 
-      format = "[░▒▓](#a3aed2)[  ](bg:#a3aed2 fg:#090c0c)[](bg:#769ff0 fg:#a3aed2)$directory[](fg:#769ff0 bg:#394260)$git_branch$git_status[](fg:#394260 bg:#212736)$nodejs$rust$golang$php[](fg:#212736 bg:#1d2230)$time[ ](fg:#1d2230)\n$character";
+      format = "[░▒▓](#a3aed2)[  ](bg:#a3aed2 fg:#090c0c)$hostname[](bg:#769ff0 fg:#a3aed2)$directory[](fg:#769ff0 bg:#394260)$git_branch$git_status[](fg:#394260 bg:#212736)$nodejs$rust$golang$php[](fg:#212736 bg:#1d2230)$time[ ](fg:#1d2230)\n$character";
+
+      hostname = {
+        disabled = false;
+        format = "[ $hostname](bg:#a3aed2 fg:#090c0c)";
+      };
 
       os = {
         disabled = false;
