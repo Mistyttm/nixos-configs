cb1b4fdb7abffeaf0a270df7c2675d5414bd24c7
Mistyttm <contact@mistyttm.dev>
2025-12-18 22:58:21 +1000

    chore(flake): remove PR branch

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     # Minecraft server management
     nix-minecraft.url = "github:Infinidoge/nix-minecraft";
     # VSCode extensions
-    nix-vscode-extensions.url = "github:dseum/nix-vscode-extensions";
+    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
     auto-cpufreq = {
       url = "github:AdnanHodzic/auto-cpufreq";
       inputs.nixpkgs.follows = "nixpkgs";
