10ba74216664fa0153d01f4258aed7080d7ae299
Mistyttm <contact@mistyttm.dev>
2025-12-23 22:24:44 +1000

    fix(puppypc): remove unnecessary device requirements for Noisetorch service

--- hosts/puppypc/system/systemd.nix
+++ hosts/puppypc/system/systemd.nix
@@ -7,17 +7,16 @@
     noisetorch = {
       description = "Noisetorch Noise Cancelling";
 
-      requires = [
-        "sys-devices-pci0000:00-0000:00:02.1-0000:03:00.0-0000:04:0c.0-0000:0e:00.0-usb1-1\\x2d5-1\\x2d5.1-1\\x2d5.1:1.0-sound-card0-controlC0.device"
-      ];
       after = [
-        "sys-devices-pci0000:00-0000:00:02.1-0000:03:00.0-0000:04:0c.0-0000:0e:00.0-usb1-1\\x2d5-1\\x2d5.1-1\\x2d5.1:1.0-sound-card0-controlC0.device"
-        "pipewire.service" # Use "pulseaudio.service" if using PulseAudio
+        "pipewire.service"
+        "pipewire-pulse.service"
       ];
+      wants = [ "pipewire.service" ];
 
       serviceConfig = {
         Type = "simple";
         RemainAfterExit = "yes";
+        ExecStartPre = "${pkgs.coreutils}/bin/sleep 3"; # Give PipeWire time to enumerate devices
         ExecStart = "${pkgs.noisetorch}/bin/noisetorch -i -s alsa_input.usb-Razer_Inc_Razer_Seiren_Mini_UC2222L03203690-00.mono-fallback -t 95";
         ExecStop = "${pkgs.noisetorch}/bin/noisetorch -u";
         Restart = "on-failure";
