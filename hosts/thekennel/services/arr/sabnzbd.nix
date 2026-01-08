2f2137856e763edbbad89b64214758719d4219fb
Mistyttm <contact@mistyttm.dev>
2026-01-08 21:19:54 +1000

    fix(thekennel): update sabnzbd service configuration for foreground mode

--- hosts/thekennel/services/arr/sabnzbd.nix
+++ hosts/thekennel/services/arr/sabnzbd.nix
@@ -1,7 +1,20 @@
-{ ... }:
+{
+  config,
+  lib,
+  ...
+}:
 {
   services.sabnzbd = {
     enable = true;
     openFirewall = true;
+  };
+
+  # Override to run in foreground mode (remove -d flag)
+  # The default module uses Type=forking with -d which fails on this system
+  systemd.services.sabnzbd = {
+    serviceConfig = {
+      ExecStart = lib.mkForce "${config.services.sabnzbd.package}/bin/sabnzbd -f ${config.services.sabnzbd.configFile}";
+      Type = lib.mkForce "simple";
+    };
   };
 }
