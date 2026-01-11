2e1bdc82148804e61012cdf57f625c0d4ff2ac6a
Mistyttm <contact@mistyttm.dev>
2026-01-11 14:34:05 +1000

    refactor(games): remove deprecated package configuration

--- global-configs/programs/games/default.nix
+++ global-configs/programs/games/default.nix
@@ -1,15 +1,5 @@
-{ pkgs, ... }:
+{ ... }:
 {
-  #  imports = [
-  #  ];
-  home.packages = with pkgs; [
-    (prismlauncher.override {
-      jdks = [
-        jdk8
-        jdk17
-        jdk21
-      ];
-    })
-    protonup-qt
-  ];
+  # Games packages are now handled by the gaming module in modules/nixos/gaming.nix
+  # Enable gaming options in your host's configuration.nix instead
 }
