73e38bb42880b807a125f15591132b4844b97d4a
Mistyttm <contact@mistyttm.dev>
2026-01-20 16:05:15 +1000

    fix(global-configs): fix eval warning for DNSSEC

--- global-configs/system/networking/dns.nix
+++ global-configs/system/networking/dns.nix
@@ -7,12 +7,16 @@
 
   services.resolved = {
     enable = true;
-    dnssec = "true";
-    domains = [ "~." ];
-    fallbackDns = [
-      "1.1.1.1"
-      "1.0.0.1"
-    ];
-    dnsovertls = "true";
+    settings = {
+      Resolve = {
+        DNSOverTLS = "true";
+        DNSSEC = "true";
+        Domains = [ "~." ];
+        FallbackDNS = [
+          "1.1.1.1"
+          "1.0.0.1"
+        ];
+      };
+    };
   };
 }
