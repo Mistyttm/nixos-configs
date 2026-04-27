d6c1b65540da63570962f01a1b7f09ae578105a8
Mistyttm <contact@mistyttm.dev>
2026-04-28 00:04:01 +1000

    refactor(thekennel): remove heimdall-local virtual host configuration

--- hosts/thekennel/services/nginx.nix
+++ hosts/thekennel/services/nginx.nix
@@ -1,7 +1,4 @@
 {
-  config,
-  pkgs,
-  lib,
   ...
 }:
 {
@@ -52,41 +49,6 @@
       locations."/" = {
         proxyPass = "http://127.0.0.1:8082";
         proxyWebsockets = true;
-      };
-    };
-
-    virtualHosts."heimdall-local" = lib.mkIf config.services.heimdall.enable {
-      listen = [
-        {
-          addr = "127.0.0.1";
-          port = 8098;
-        }
-      ];
-
-      root = "${config.services.heimdall.package}/share/heimdall/public";
-
-      extraConfig = ''
-        index index.php;
-        charset utf-8;
-        client_max_body_size 30M;
-      '';
-
-      locations."/" = {
-        tryFiles = "$uri $uri/ /index.php?$query_string";
-      };
-
-      locations."~ \\.php$" = {
-        extraConfig = ''
-          fastcgi_pass unix:${config.services.phpfpm.pools.heimdall.socket};
-          fastcgi_index index.php;
-          fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-          include ${pkgs.nginx}/conf/fastcgi_params;
-          fastcgi_param PHP_VALUE "upload_max_filesize=30M \n post_max_size=30M";
-        '';
-      };
-
-      locations."~ /\\.(?!well-known).*" = {
-        return = "404";
       };
     };
   };
