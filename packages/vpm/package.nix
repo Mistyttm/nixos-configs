b76e1227a79b9f487a631a612c90c40ba95f0dfa
Mistyttm <contact@mistyttm.dev>
2026-04-30 17:31:22 +1000

    fix(packages/vpm): add update script for vpm package

--- packages/vpm/package.nix
+++ packages/vpm/package.nix
@@ -10,6 +10,8 @@ buildDotnetGlobalTool {
 
   nugetHash = "sha256-Pz8KBpjmpzx+6gD4nqGVBEp5z4UX6hFqZHGy8hJCD4k=";
 
+  passthru.updateScript = ./update.sh;
+
   meta = {
     description = "VRChat Package Manager CLI";
     homepage = "https://vcc.docs.vrchat.com/vpm/cli";
