922f6d1c6993caeccdc37758da0923583859ecbb
Mistyttm <contact@mistyttm.dev>
2026-05-03 13:30:19 +1000

    fix(audio.nix): remove unused Bluetooth audio roles from WirePlumber configuration

--- global-configs/system/DE/audio.nix
+++ global-configs/system/DE/audio.nix
@@ -24,9 +24,7 @@
       "bluez5.enable-hw-volume" = true;
       "bluez5.roles" = [
         "hsp_hs"
-        "hsp_ag"
         "hfp_hf"
-        "hfp_ag"
       ];
     };
     # Prevent unclean Bluetooth disconnects from crashing WirePlumber and
