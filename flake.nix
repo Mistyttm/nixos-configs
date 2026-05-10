a183d2aab2e5ff6ac2ee4735bc62623e8b1d4190
Mistyttm <contact@mistyttm.dev>
2026-05-10 23:10:00 +1000

    chore: Swap to main branch of Lix

--- flake.nix
+++ flake.nix
@@ -129,7 +129,7 @@
         sops-nix.nixosModules.sops
         nix-topology.nixosModules.default
         nix-index-database.nixosModules.default
-        lix-module.nixosModules.lixFromNixpkgs
+        lix-module.nixosModules.default
         {
           programs.nix-index-database.comma.enable = true;
         }
