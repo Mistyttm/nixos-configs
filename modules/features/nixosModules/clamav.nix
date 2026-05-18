bed45a263f435de01d6d4fe206f354c681b75d17
Mistyttm <contact@mistyttm.dev>
2026-05-18 23:49:18 +1000

    feat(clamav): add initial configuration for ClamAV and ClamTK services

---
+++ modules/features/nixosModules/clamav.nix
@@ -0,0 +1,33 @@
+{ ... }:
+{
+  flake.nixosModules.clamav =
+    { pkgs, ... }:
+    {
+      environment.systemPackages = with pkgs; [
+        clamav
+        clamtk
+      ];
+
+      services.clamav = {
+        enable = true;
+        daemon = {
+          enable = true;
+          config = {
+            "LocalSocket" = "/var/run/clamav/clamd.sock";
+            "TCPSocket" = 3310;
+          };
+        };
+        scanner = {
+          enable = true;
+        };
+        updater = {
+          enable = true;
+          config = {
+            "DatabaseDirectory" = "/var/lib/clamav";
+            "LocalSocket" = "/var/run/clamav/clamd.sock";
+            "TCPSocket" = 3310;
+          };
+        };
+      };
+    };
+}
