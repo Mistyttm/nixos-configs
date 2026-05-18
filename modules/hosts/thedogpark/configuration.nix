051ac6ba8c7b1c20ec64dc0723e81931b26abb76
Mistyttm <contact@mistyttm.dev>
2026-05-19 00:00:49 +1000

    feat(thedogpark): add systemd service configuration for Vite production site

--- modules/hosts/thedogpark/configuration.nix
+++ modules/hosts/thedogpark/configuration.nix
@@ -18,6 +18,7 @@
         mullvad
         fail2ban
         matrix
+        motd
       ];
 
       networking.hostName = "thedogpark";
@@ -87,6 +88,23 @@
         permittedInsecurePackages = [
           "olm-3.2.16"
         ];
+      };
+
+      systemd.services.mistyttmpersonalsite = {
+        description = "Build and run Vite for the MistyTTM Personal Site in production mode";
+        after = [ "network.target" ];
+        wantedBy = [ "multi-user.target" ];
+
+        serviceConfig = {
+          Type = "simple";
+          ExecStart = "${pkgs.bash}/bin/bash -c 'cd /home/misty/Documents/mistyttmpersonalsite && ${pkgs.nodejs}/bin/npx vite --mode production --port 8080'";
+          WorkingDirectory = "/home/misty/Documents/mistyttmpersonalsite";
+          Restart = "on-failure";
+          # AmbientCapabilities = "CAP_NET_BIND_SERVICE";
+          User = "misty";
+          Group = "users";
+          Environment = "PATH=${pkgs.nodejs}/bin:${pkgs.nodejs}/lib/node_modules/.bin:${pkgs.bash}/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin";
+        };
       };
 
       system.stateVersion = "25.11";
