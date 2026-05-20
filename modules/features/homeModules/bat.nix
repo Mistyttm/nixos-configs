2047629e4cf2b537c1819145f1406cebf776a92d
Mistyttm <contact@mistyttm.dev>
2026-05-20 15:36:43 +1000

    refactor(bat): move to homeModules

---
+++ modules/features/homeModules/bat.nix
@@ -0,0 +1,11 @@
+{ ... }:
+{
+  flake.homeModules.bat =
+    { pkgs, ... }:
+    {
+      programs.bat = {
+        enable = true;
+        extraPackages = with pkgs.bat-extras; [ core ];
+      };
+    };
+}
