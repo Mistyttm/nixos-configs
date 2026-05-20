36aa55e9cff1142c904fa7782e0507e8d6f4d292
Mistyttm <contact@mistyttm.dev>
2026-05-20 20:54:06 +1000

    refactor(starship): Add per-host starship configs

--- modules/features/homeModules/starship/default.nix
+++ modules/features/homeModules/starship/default.nix
@@ -1,16 +1,25 @@
-{
-  self,
-  inputs,
-  config,
-  ...
-}:
+{ self, ... }:
 {
   flake.homeModules.starship =
-    { pkgs, ... }:
     {
-      programs.starship = {
-        enable = true;
-        enableZshIntegration = true;
+      config,
+      pkgs,
+      lib,
+      ...
+    }:
+    {
+      options.programs.puppy.starship.hostname = lib.mkOption {
+        type = lib.types.str;
+        description = "Hostname used to select the Starship package variant";
+      };
+
+      config = {
+        programs.starship = {
+          enable = true;
+          enableZshIntegration = true;
+          package =
+            self.packages.${pkgs.stdenv.hostPlatform.system}."${config.programs.puppy.starship.hostname}Starship";
+        };
       };
     };
 }
