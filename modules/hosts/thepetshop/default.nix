4fb588e9e49cbdc09d8cf20838ac2f504f28044c
Mistyttm <contact@mistyttm.dev>
2026-05-22 14:01:49 +1000

    fix(build): update ISO build process to use new package structure for improved architecture support

--- modules/hosts/thepetshop/default.nix
+++ modules/hosts/thepetshop/default.nix
@@ -13,4 +13,11 @@
       self.nixosModules.thepetshopInstaller
     ];
   };
+
+  flake.packages = {
+    x86_64-linux.thepetshop-iso =
+      self.nixosConfigurations."thepetshop-x86_64".config.system.build.images.iso-installer;
+    aarch64-linux.thepetshop-iso =
+      self.nixosConfigurations."thepetshop-aarch64".config.system.build.images.iso-installer;
+  };
 }
