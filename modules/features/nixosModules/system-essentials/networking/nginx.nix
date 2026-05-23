ace6f3d8cb8e21b4d0f9b23013d442da1b8e3a1a
Mistyttm <contact@mistyttm.dev>
2026-05-23 23:46:11 +1000

    fix(nginx): update proxy header for discord-media virtual host

--- modules/features/nixosModules/system-essentials/networking/nginx.nix
+++ modules/features/nixosModules/system-essentials/networking/nginx.nix
@@ -101,7 +101,7 @@
               locations."/" = {
                 proxyPass = "http://localhost:${toString config.services.mautrix-discord.settings.appservice.port}";
                 extraConfig = ''
-                  proxy_set_header Host $host;
+                  proxy_set_header Host "jellyfin.mistyttm.dev";
                   proxy_set_header X-Real-IP $remote_addr;
                   proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
                   proxy_set_header X-Forwarded-Proto $scheme;
