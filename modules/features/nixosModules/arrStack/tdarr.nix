cd08de3d9e2dd29e73af3c1d4c04426d6605646a
Mistyttm <contact@mistyttm.dev>
2026-05-24 23:01:22 +1000

    fix(tdarr): update tdarrFixPkgs to use sha256 for dependency integrity

--- modules/features/nixosModules/arrStack/tdarr.nix
+++ modules/features/nixosModules/arrStack/tdarr.nix
@@ -8,7 +8,7 @@ let
     import
       (builtins.fetchTarball {
         url = "https://github.com/Mistyttm/nixpkgs/archive/72fc7a8f3fdf561294ea7b1ace802484a24ced4b.tar.gz";
-        hash = "";
+        sha256 = "";
       })
       {
         system = builtins.currentSystem;
