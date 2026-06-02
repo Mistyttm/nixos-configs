95346f456eec11d6700a36e3953975a47959a673
Mistyttm <contact@mistyttm.dev>
2026-06-02 23:37:20 +1000

    feat(steam): Add proton-cachyos and proton-ge-rtsp to extraCompatPackages

--- modules/features/nixosModules/gaming/steam.nix
+++ modules/features/nixosModules/gaming/steam.nix
@@ -1,34 +1,35 @@
-{ ... }:
-{
-  flake.nixosModules.steam =
-    { pkgs, ... }:
-    {
-      programs.steam = {
-        enable = true;
-        remotePlay.openFirewall = true;
-        dedicatedServer.openFirewall = true;
-        localNetworkGameTransfers.openFirewall = true;
-        extraPackages = with pkgs; [
-          gamescope
-          mangohud
-        ];
-        extraCompatPackages = with pkgs; [ proton-ge-bin ];
-        package = pkgs.steam.override {
-          extraEnv = {
-            OBS_VKCAPTURE = true;
-          };
+{...}: {
+  flake.nixosModules.steam = {pkgs, ...}: {
+    programs.steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+      localNetworkGameTransfers.openFirewall = true;
+      extraPackages = with pkgs; [
+        gamescope
+        mangohud
+      ];
+      extraCompatPackages = with pkgs; [
+        proton-ge-bin
+        proton-cachyos
+        proton-ge-rtsp
+      ];
+      package = pkgs.steam.override {
+        extraEnv = {
+          OBS_VKCAPTURE = true;
         };
       };
-
-      programs.gamemode = {
-        enable = true;
-      };
-
-      environment.systemPackages = with pkgs; [
-        protonplus
-        protontricks
-        wineWow64Packages.waylandFull
-        winetricks
-      ];
     };
+
+    programs.gamemode = {
+      enable = true;
+    };
+
+    environment.systemPackages = with pkgs; [
+      protonplus
+      protontricks
+      wineWow64Packages.waylandFull
+      winetricks
+    ];
+  };
 }
