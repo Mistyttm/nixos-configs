2464b4d8e84d80311f3b6a5658a78123e9ca1045
Mistyttm <contact@mistyttm.dev>
2026-06-05 16:32:21 +1000

    fix(packages/vanilla-wiiu): Add custom update script

--- packages/vanilla-wiiu/package.nix
+++ packages/vanilla-wiiu/package.nix
@@ -3,7 +3,6 @@
   stdenv,
   fetchFromGitHub,
   cmake,
-  nix-update-script,
   ffmpeg,
   networkmanager,
   SDL2,
@@ -21,75 +20,74 @@
   fetchgit,
   libx11,
   autoAddDriverRunpath,
-}:
-let
+}: let
   drc-hostap-src = fetchgit {
     url = "https://github.com/rolandoislas/drc-hostap.git";
     rev = "418e5e206786de2482864a0ec3a59742a33b6623"; # find the commit from the CMakeLists.txt
     hash = "sha256-0LZLNhGF5OC0AkVRFyP+vtjPJ5VEeIJF9ZfBpoJZdH4=";
   };
 in
-stdenv.mkDerivation (finalAttrs: {
-  pname = "vanilla-wiiu";
-  version = "continuous";
-  __structuredAttrs = true;
-  strictDeps = true;
+  stdenv.mkDerivation (finalAttrs: {
+    pname = "vanilla-wiiu";
+    version = "continuous-2026-06-05";
+    __structuredAttrs = true;
+    strictDeps = true;
 
-  src = fetchFromGitHub {
-    owner = "vanilla-wiiu";
-    repo = "vanilla";
-    tag = finalAttrs.version;
-    hash = "sha256-dThwQNNtP5V7PctSH0i7zgks73F15YHLEEWGg5BWrJI=";
-    fetchSubmodules = true;
-  };
+    src = fetchFromGitHub {
+      owner = "vanilla-wiiu";
+      repo = "vanilla";
+      rev = "91ab3a0ccd1741f89d49b696e1da61ee0775f919";
+      hash = "sha256-dThwQNNtP5V7PctSH0i7zgks73F15YHLEEWGg5BWrJI=";
+      fetchSubmodules = true;
+    };
 
-  patches = [ ./hostap-nix-source.patch ];
+    patches = [./hostap-nix-source.patch];
 
-  postPatch = ''
-    substituteInPlace pipe/linux/CMakeLists.txt \
-      --replace-fail '@DRC_HOSTAP_SRC@' '${drc-hostap-src}'
+    postPatch = ''
+      substituteInPlace pipe/linux/CMakeLists.txt \
+        --replace-fail '@DRC_HOSTAP_SRC@' '${drc-hostap-src}'
 
-    substituteInPlace gui/ui/ui_sdl.c \
-      --replace-fail '<SDL_image.h>' '<SDL2/SDL_image.h>' \
-      --replace-fail '<SDL_ttf.h>' '<SDL2/SDL_ttf.h>'
-  '';
+      substituteInPlace gui/ui/ui_sdl.c \
+        --replace-fail '<SDL_image.h>' '<SDL2/SDL_image.h>' \
+        --replace-fail '<SDL_ttf.h>' '<SDL2/SDL_ttf.h>'
+    '';
 
-  nativeBuildInputs = [
-    cmake
-    pkg-config
-    git
-  ];
+    nativeBuildInputs = [
+      cmake
+      pkg-config
+      git
+    ];
 
-  buildInputs = [
-    ffmpeg
-    networkmanager
-    libnl
-    glib
-    SDL2
-    SDL2_ttf
-    SDL2_image
-    openssl
-    libwebp
-    libtiff
-    polkit
-    libxml2
-    libx11
-    autoAddDriverRunpath
-  ];
+    buildInputs = [
+      ffmpeg
+      networkmanager
+      libnl
+      glib
+      SDL2
+      SDL2_ttf
+      SDL2_image
+      openssl
+      libwebp
+      libtiff
+      polkit
+      libxml2
+      libx11
+      autoAddDriverRunpath
+    ];
 
-  cmakeFlags = [
-    "-DCMAKE_BUILD_TYPE=Release"
-  ];
+    cmakeFlags = [
+      "-DCMAKE_BUILD_TYPE=Release"
+    ];
 
-  passthru.updateScript = nix-update-script { };
+    passthru.updateScript = ./update.sh;
 
-  meta = {
-    description = "";
-    homepage = "https://github.com/vanilla-wiiu/vanilla";
-    changelog = "https://github.com/vanilla-wiiu/vanilla/releases/tag/${finalAttrs.src.tag}";
-    license = lib.licenses.gpl2Only;
-    maintainers = with lib.maintainers; [ mistyttm ];
-    mainProgram = "vanilla-wiiu";
-    platforms = lib.platforms.all;
-  };
-})
+    meta = {
+      description = "";
+      homepage = "https://github.com/vanilla-wiiu/vanilla";
+      changelog = "https://github.com/vanilla-wiiu/vanilla/releases/tag/${finalAttrs.src.tag}";
+      license = lib.licenses.gpl2Only;
+      maintainers = with lib.maintainers; [mistyttm];
+      mainProgram = "vanilla-wiiu";
+      platforms = lib.platforms.all;
+    };
+  })
---
+++ packages/vanilla-wiiu/update.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p curl jq common-updater-scripts git
+
+set -euo pipefail
+
+latest_rev="$(
+  curl -fsSL \
+    https://api.github.com/repos/vanilla-wiiu/vanilla/releases/tags/continuous \
+  | jq -r '.target_commitish'
+)"
+
+latest_date="$(
+  curl -fsSL \
+    "https://api.github.com/repos/vanilla-wiiu/vanilla/commits/${latest_rev}" \
+  | jq -r '.commit.committer.date' \
+  | cut -dT -f1
+)"
+
+update-source-version vanilla-wiiu \
+  "continuous-${latest_date}" \
+  --rev="$latest_rev"
