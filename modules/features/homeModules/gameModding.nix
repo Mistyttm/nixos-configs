aea83cf805076ad28936bb0909fb88d91e790561
Mistyttm <contact@mistyttm.dev>
2026-06-06 23:50:10 +1000

    fix(gamemodding): Add back dmm, patch out failing test

--- modules/features/homeModules/gameModding.nix
+++ modules/features/homeModules/gameModding.nix
@@ -1,18 +1,17 @@
-{ ... }:
-{
-  flake.homeModules.gameModding =
-    { pkgs, ... }:
-    {
-      home.packages =
-        with pkgs;
-        [
-          blockbench
-          bs-manager
-          satisfactorymodmanager
-          r2modman
-        ]
-        ++ [
-          # inputs.deadlock-mod-manager.packages.${pkgs.stdenv.hostPlatform.system}.nightly
-        ];
-    };
+{inputs, ...}: {
+  flake.homeModules.gameModding = {pkgs, ...}: {
+    home.packages = with pkgs;
+      [
+        blockbench
+        bs-manager
+        satisfactorymodmanager
+        r2modman
+      ]
+      ++ [
+        (inputs.deadlock-mod-manager.packages.${pkgs.stdenv.hostPlatform.system}.nightly.overrideAttrs
+          (_finalAttrs: oldAttrs: {
+            checkFlags = oldAttrs.checkFlags or [] ++ ["--skip=mod_manager::steam_manager::tests::set_steam_dir_rejects_invalid_directory"];
+          }))
+      ];
+  };
 }
