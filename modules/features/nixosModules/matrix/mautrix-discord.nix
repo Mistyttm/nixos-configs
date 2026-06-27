bbe270508faf3d525f648008513dbe97d697b019
Mistyttm <contact@mistyttm.dev>
2026-06-27 20:54:56 +1000

    fix(mautrix-discord): add db type

--- modules/features/nixosModules/matrix/mautrix-discord.nix
+++ modules/features/nixosModules/matrix/mautrix-discord.nix
@@ -1,36 +1,33 @@
-{ ... }:
-{
-  flake.nixosModules.mautrix-discord =
-    { ... }:
-    let
-      synapsePort = 8008;
-    in
-    {
-      services.mautrix-discord = {
-        enable = true;
-        registerToSynapse = true;
-        settings = {
-          homeserver = {
-            address = "http://localhost:${toString synapsePort}";
-            domain = "mistyttm.dev";
+{...}: {
+  flake.nixosModules.mautrix-discord = {...}: let
+    synapsePort = 8008;
+  in {
+    services.mautrix-discord = {
+      enable = true;
+      registerToSynapse = true;
+      settings = {
+        homeserver = {
+          address = "http://localhost:${toString synapsePort}";
+          domain = "mistyttm.dev";
+        };
+        appservice = {
+          address = "http://localhost:8009";
+          port = 8009;
+          hostname = "0.0.0.0";
+          database = {
+            type = "postgres";
+            uri = "postgres:///mautrix-discord?host=/run/postgresql&sslmode=disable";
           };
-          appservice = {
-            address = "http://localhost:8009";
-            port = 8009;
-            hostname = "0.0.0.0";
-            database = {
-              uri = "postgres:///mautrix-discord?host=/run/postgresql&sslmode=disable";
-            };
+        };
+        bridge = {
+          direct-media = {
+            server_name = "discord-media.mistyttm.dev";
           };
-          bridge = {
-            direct-media = {
-              server_name = "discord-media.mistyttm.dev";
-            };
-            permissions = {
-              "mistyttm.dev" = "user";
-            };
+          permissions = {
+            "mistyttm.dev" = "user";
           };
         };
       };
     };
+  };
 }
