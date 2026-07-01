dff08c1a3a46588456d18c9cf51235add01e373d
Mistyttm <contact@mistyttm.dev>
2026-07-01 16:07:58 +1000

    fix(gui-packages): override vesktop's pnpm version

--- modules/features/homeModules/gui-packages.nix
+++ modules/features/homeModules/gui-packages.nix
@@ -1,26 +1,25 @@
-{ ... }:
-{
-  flake.homeModules.gui-packages =
-    { pkgs, ... }:
-    {
-      home.packages = with pkgs; [
-        libsecret
-        slack
-        zoom-us
-        obsidian
-        vlc
-        thunderbird-esr
-        libreoffice-qt
-        zenity
-        libnotify
-        teams-for-linux
-        python3
-        nil
-        vesktop
-        heroic
-        gimp
-        blender
-        qbittorrent
-      ];
-    };
+{...}: {
+  flake.homeModules.gui-packages = {pkgs, ...}: {
+    home.packages = with pkgs; [
+      libsecret
+      slack
+      zoom-us
+      obsidian
+      vlc
+      thunderbird-esr
+      libreoffice-qt
+      zenity
+      libnotify
+      teams-for-linux
+      python3
+      nil
+      (vesktop.override {
+        pnpm_10_29_2 = pkgs.pnpm_10;
+      })
+      heroic
+      gimp
+      blender
+      qbittorrent
+    ];
+  };
 }
