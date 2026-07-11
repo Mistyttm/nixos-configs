2f85256510831899c3d0b7d532245f3ac8339281
Mistyttm <contact@mistyttm.dev>
2026-07-11 22:05:22 +1000

    refactor(protonmail): edit service, use gui

--- modules/features/homeModules/protonmail-bridge.nix
+++ modules/features/homeModules/protonmail-bridge.nix
@@ -1,11 +1,19 @@
-{...}: {
-  flake.homeModules.protonmail = {pkgs, ...}: {
-    home.packages = with pkgs; [
-      protonmail-bridge-gui
-    ];
+{lib, ...}: {
+  flake.homeModules.protonmail = {
+    config,
+    pkgs,
+    ...
+  }: {
     services.protonmail-bridge = {
       enable = true;
-      package = pkgs.protonmail-bridge;
+      package = pkgs.protonmail-bridge-gui;
+      logLevel = "info";
+    };
+
+    systemd.user.services.protonmail-bridge.Service = {
+      After = lib.mkForce ["graphical-session.target"];
+      PartOf = lib.mkForce ["graphical-session.target"];
+      ExecStart = lib.mkForce "${lib.getExe config.services.protonmail-bridge.package} --log-level ${config.services.protonmail-bridge.logLevel}";
     };
   };
 }
