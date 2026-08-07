d8375ea3f7e7073d6c1c8c7c6868ebf78eec0078
Mistyttm <contact@mistyttm.dev>
2026-08-07 10:54:03 +1000

    fix(vpn-confinement): Properly remove prowlarr

--- modules/features/nixosModules/system-essentials/networking/vpn-confinement.nix
+++ modules/features/nixosModules/system-essentials/networking/vpn-confinement.nix
@@ -78,10 +78,6 @@
           from = 8080;
           to = 8080;
         } # qBittorrent WebUI
-        {
-          from = 9696;
-          to = 9696;
-        } # Prowlarr WebUI
       ];
 
       # torrent listening port, forwarded through the VPN itself
