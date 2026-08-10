b59b4f5bbd4cd2a2de347447ee70e89308592ec8
Mistyttm <contact@mistyttm.dev>
2026-08-10 10:24:13 +1000

    fix(homepage): fix wireguard access

--- modules/features/nixosModules/system-essentials/networking/nginx.nix
+++ modules/features/nixosModules/system-essentials/networking/nginx.nix
@@ -74,7 +74,7 @@
             ];
 
             locations."/" = {
-              proxyPass = "http://127.0.0.1:8082";
+              proxyPass = "http://127.0.0.1:3000";
               proxyWebsockets = true;
             };
           };
