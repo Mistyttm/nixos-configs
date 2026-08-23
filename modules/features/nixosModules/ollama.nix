7c4bae0a008f79f03f49bdc88ba7c0a794d2b813
Mistyttm <contact@mistyttm.dev>
2026-08-23 22:11:13 +1000

    fix(ollama): attempt to change the context length

--- modules/features/nixosModules/ollama.nix
+++ modules/features/nixosModules/ollama.nix
@@ -14,7 +14,7 @@
 
         OLLAMA_FLASH_ATTENTION = "1";
         OLLAMA_KV_CACHE_TYPE = "q8_0";
-        OLLAMA_CONTEXT_LENGTH = "65536";
+        OLLAMA_CONTEXT_LENGTH = "32768";
       };
     };
   };
