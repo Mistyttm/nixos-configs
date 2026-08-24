45fd4e978bb0d4eb3607a6324a9b25283afc5a9d
Mistyttm <contact@mistyttm.dev>
2026-08-24 16:12:56 +1000

    fix(draupnir): swap to node22

--- modules/features/nixosModules/matrix/draupnir.nix
+++ modules/features/nixosModules/matrix/draupnir.nix
@@ -1,12 +1,16 @@
 {self, ...}: {
-  flake.nixosModules.draupnir = {config, ...}: {
+  flake.nixosModules.draupnir = {
+    config,
+    pkgs,
+    ...
+  }: {
     sops.secrets."mjolnir_access_token" = {
       sopsFile = self.secrets.synapse;
     };
 
     services.draupnir = {
       enable = true;
-
+      package = pkgs.draupnir.override {nodejs_24 = pkgs.nodejs_22;};
       settings = {
         homeserverUrl = "http://localhost:8008";
         rawHomeserverUrl = "https://mistyttm.dev";
