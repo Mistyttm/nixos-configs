b1903021f55e2f0776c62f139ca6eac0428aa8f8
Mistyttm <contact@mistyttm.dev>
2026-09-16 12:19:15 +1000

    fix(linux-wallpaperengine): fix crash when changing wallpapers

--- modules/features/homeModules/linux-wallpaperengine/linux-wallpaperengine.nix
+++ modules/features/homeModules/linux-wallpaperengine/linux-wallpaperengine.nix
@@ -11,7 +11,7 @@
     services.linux-wallpaperengine = {
       enable = true;
       package = pkgs.linux-wallpaperengine.overrideAttrs (_final: prev: {
-        patches = (prev.patches or []) ++ [./linux-wallpaperengine-playlist-path.patch];
+        patches = (prev.patches or []) ++ [./linux-wallpaperengine-playlist-path.patch ./linux-wallpaperengine-albumart-listener-leak.patch];
       });
       assetsPath = "${config.xdg.dataHome}/Steam/steamapps/common/wallpaper_engine/assets";
       wallpapers = [
